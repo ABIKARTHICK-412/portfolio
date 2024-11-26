fa33387a49f2ca09df96609e85f6961f9cb11145
ABIKARTHICK-412 <146557352+ABIKARTHICK-412@users.noreply.github.com>
2024-11-26 23:46:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=============================typing animation========================*/
+var typed = new Typed(".typing", {
+strings:["","  Game developer", "  game Programmer", "  Game Artist", ],
+typeSpeed: 150,
+BackSpeed: 60,
+loop:true
+})
